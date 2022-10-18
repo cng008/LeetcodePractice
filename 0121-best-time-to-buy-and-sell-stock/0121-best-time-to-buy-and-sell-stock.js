fab11484c93e95cb76eeb82db230145612ebf0ce
Christien <92824407+cng008@users.noreply.github.com>
2022-10-18 16:47:19 -0700

    Time: 140 ms (52.31%), Space: 51.5 MB (85.05%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -5,16 +5,16 @@
 const maxProfit = (prices) => {
     let max_profit = 0, 
         windowStart = 0, // buy
-        windowEnd = 0; // sell
+        windowEnd = 1; // sell
     
     while (windowEnd < prices.length) {
-        windowEnd++ // shrink the window ahead
         if (prices[windowStart] < prices[windowEnd]){
             let profit = prices[windowEnd] - prices[windowStart]
             max_profit = Math.max(max_profit, profit)
         } else { // if num at windowEnd is less than num at windowStart
             windowStart = windowEnd
         }
+        windowEnd++
     }
     
     return max_profit
