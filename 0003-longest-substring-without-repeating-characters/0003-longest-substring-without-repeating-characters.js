f45f8878a0296724594b64645c5caefe67aefce3
Christien <92824407+cng008@users.noreply.github.com>
2022-10-18 17:32:51 -0700

    Time: 111 ms (87.18%), Space: 47.5 MB (59.72%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ * assumptions: str is not empty, lowercase letters only
+ * output: integer, length of longest substring of distinct characters
+ */
+const lengthOfLongestSubstring = (s) => {
+    let maxLength = 0,
+        windowStart = 0,
+        charIndexMap = {};
+    
+    for (let windowEnd = 0; windowEnd < s.length; windowEnd++){
+        const rightChar = s[windowEnd]
+        if (rightChar in charIndexMap) {
+            // shrink window from start
+            // update start to include repeating char
+            windowStart = Math.max(windowStart, charIndexMap[rightChar]+1)
+        }
+        // insert rightChar into the map
+        charIndexMap[rightChar] = windowEnd
+        // remember the max length so far
+        maxLength = Math.max(maxLength, windowEnd - windowStart + 1)
+    }
+    return maxLength
+};
