0fe81b0ced326debddead99fbe318b74c6cf5dfe
Christien <92824407+cng008@users.noreply.github.com>
2022-10-18 17:16:30 -0700

    Time: 101 ms (65.25%), Space: 44.1 MB (80.86%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -4,21 +4,15 @@
  */
 const fizzBuzz = (n) => {
     let result = []
-    for (let i = 1; i <= n; i++){
-        if (i % 3 === 0 && i % 5 === 0) {
-            // 'FizzBuzz' if % 3 AND 5
-            result.push('FizzBuzz')
-        } else if (i % 3 === 0) {
-            // 'Fizz' if % 3
-            result.push('Fizz')
-        } else if (i % 5 === 0) {
-            // 'Buzz' if % 5
-            result.push('Buzz')
-        } else {
-            // 'num' if neither
-            result.push(i.toString())
-        }
+    for (let i=1; i<=n; i++){
+        let currentString = "";
+        
+        if(i % 3 === 0) currentString += "Fizz";
+        if(i % 5 === 0) currentString += "Buzz";
+
+        if(currentString === "") currentString += i;
+
+        result.push(currentString);
     }
-    
     return result
 };
