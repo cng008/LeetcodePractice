7126a967ea17b4d43502ab81926d452c529b6162
Christien <92824407+cng008@users.noreply.github.com>
2022-10-20 16:38:28 -0700

    Time: 127 ms (40.77%), Space: 44.6 MB (33.28%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.js
+++ 0733-flood-fill/0733-flood-fill.js
@@ -28,3 +28,25 @@ const fillDFS = (image, x, y, oldColor, newColor) => {
     fillDFS(image, x, y + 1, oldColor, newColor) // right cell
     fillDFS(image, x, y - 1, oldColor, newColor) // left cell
 }
+
+// BFS
+// const fillBFS = (image, x, y, oldColor, newColor) => {
+//     const neighbors = [[x,y]]
+//     while (neighbors.length > 0){
+//         const cell = neighbors.shift()
+//         const row = cell[0]
+//         const col = cell[1]
+//     }
+//     // ensure cell is valid
+//     if (x < 0 || x >= image.length || y < 0 || y >= image[0].length) return
+//     // is not the value we clicked on
+//     if (image[x][y] !== oldColor) return
+    
+//     image[x][y] = newColor // update cell to new color
+    
+//     // recursively visit surrounding cells
+//     fillBFS(image, x + 1, y, oldColor, newColor) // lower cell
+//     fillBFS(image, x - 1, y, oldColor, newColor) // upper cell
+//     fillBFS(image, x, y + 1, oldColor, newColor) // right cell
+//     fillBFS(image, x, y - 1, oldColor, newColor) // left cell
+// }
