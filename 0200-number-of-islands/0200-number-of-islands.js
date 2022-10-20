526266cb2b18b43b4ed6be3e7db4c9b1d07e3bc2
Christien <92824407+cng008@users.noreply.github.com>
2022-10-20 16:15:21 -0700

    Time: 139 ms (51.01%), Space: 49.6 MB (38.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -14,24 +14,32 @@ const numIslands = (grid) => {
                 // island found
                 islandCount++;
                 // visit surrounding cells
-                visitIslandsDFS(grid, i, j)
+                visitIslandsBFS(grid, i, j)
             }
         }
     }
     return islandCount;
 };
 
-const visitIslandsDFS = (grid, x, y) => {
+const visitIslandsBFS = (grid, x, y) => {
+    // start queue
+    const neighbors = [[x,y]]
+    while (neighbors.length > 0){
+        // get first from queue
+        const cell = neighbors.shift()
+        const row = cell[0]
+        const col = cell[1]
     // if not a valid cell, return
-    if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) return;
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return;
     // if cell is water (0), return
-    if (grid[x][y] === '0') return;
+    if (grid[row][col] === '0') return;
     // update input matrix after visiting
-    grid[x][y] = '0';
+    grid[row][col] = '0';
 
     // recursively visit surrounding cells until there are no surrounding islands
-    visitIslandsDFS(grid, x + 1, y); // lower cell
-    visitIslandsDFS(grid, x - 1, y); // upper cell
-    visitIslandsDFS(grid, x, y + 1); // right cell
-    visitIslandsDFS(grid, x, y - 1); // left cell
+    visitIslandsBFS(grid, row + 1, col); // lower cell
+    visitIslandsBFS(grid, row - 1, col); // upper cell
+    visitIslandsBFS(grid, row, col + 1); // right cell
+    visitIslandsBFS(grid, row, col - 1); // left cell
+    }
 }
