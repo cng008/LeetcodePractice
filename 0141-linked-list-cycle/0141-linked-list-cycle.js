61c55ca55df3e986c8873794918b5cf872c99759
Christien <92824407+cng008@users.noreply.github.com>
2022-10-24 20:54:28 -0700

    Time: 129 ms (32.52%), Space: 45.7 MB (16.07%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ * Time: O(n) -> traverse entire linked list
+ * Space: O(1) -> no extra memory
+ */
+const hasCycle = (head) => {
+    // start at the head for both pointers
+    let slow = head,
+        fast = head
+    
+    // only need to check fast pointer bc it will be the one to reach first
+    while (fast && fast.next){ 
+        slow = slow.next // moves 1
+        fast = fast.next.next // moves 2
+        
+        if (fast === slow){
+            return true // cycle found
+        }
+    }
+    return false
+};
