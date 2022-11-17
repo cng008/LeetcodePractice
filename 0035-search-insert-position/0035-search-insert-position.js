6c87e86b0d51a7b237a20467a3b7dfe4c8688fc0
Christien <92824407+cng008@users.noreply.github.com>
2022-11-16 21:22:16 -0800

    Time: 110 ms (17.01%), Space: 42 MB (70.42%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ * Time: O(log n) -> reduces the search range by half at every step
+ * Space: O(1) -> constant space
+ */
+
+// Modified Binary Search
+const searchInsert = (nums, target) => {
+    if (target > nums[nums.length - 1]){ // target is bigger than the biggest element
+        return nums.length; // insert at end of array
+    }
+    
+    let l = 0,
+        r = nums.length - 1;
+    
+    while (l <= r){
+        const mid = Math.floor(l + (r - l) / 2)
+        if (target > nums[mid]){
+            l = mid + 1;
+        } else if (target < nums[mid]){
+            r = mid - 1;
+        } else {
+            return mid; // found target
+        }
+    }
+    // since the loop is running until 'start <= end', so at the end of the while loop, 'start === end+1' we are not able to find the element in the given array, so the next big number will be arr[start]
+    return l;
+};
