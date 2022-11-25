c5505b4b59f8320ba9f22d9a3fdfbbe518353b06
Christien <92824407+cng008@users.noreply.github.com>
2022-11-25 12:12:44 -0800

    Time: 94 ms (85.94%), Space: 43.5 MB (20.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,18 +3,29 @@
  * @param {number} target
  * @return {number[]}
  * Time: O(n) -> iterate through input array
- * Space: O(n) -> hash map
+ * Space: O(n) -> hash map, sacrificed space for time improvement
  */
 const twoSum = (nums, target) => {
     const map = new Map();
-    
     for (let i = 0; i < nums.length; i++) {
-        if (map.has(target - nums[i])) { // difference of target exists in map
-            return [map.get(target - nums[i]), i]; // returns indices
-        } else {
-            map.set(nums[i], i); // set number as key and indice as value
+        if (map.has(target - nums[i])){ // difference of target exists in map
+            return [map.get(target - nums[i]), i] // returns indices
         }
+        map.set(nums[i], i) // set number as key and indice as value
     }
-    return []; // edge case
 };
 
+
+/** BRUTE FORCE
+* Time: O(N^2) | Space: O(1)
+*/
+// const twoSum = (nums, target) => {
+//     for (let i = 0; i < nums.length; i++){
+//         for (let j = 0; j < nums.length; j++){
+//             if (nums[i] + nums[j] === target){
+//                 return [i, j]
+//             }
+//         }
+//     }
+//     // assumed that there is a solution
+// };
