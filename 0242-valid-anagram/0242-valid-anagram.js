9327107e13537cb0cdcbbee15aa16082be839d75
Christien <92824407+cng008@users.noreply.github.com>
2022-11-24 21:24:12 -0800

    Time: 114 ms (75.72%), Space: 44.4 MB (47.76%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,15 +7,16 @@
  */
 const isAnagram = function(s, t) {
     if (s.length !== t.length) return false; // edge case
-    const newMap = new Map();
+    
+    const map = new Map();
     
     for (let char of s){
         // add to map, add 1 to value if already exists in map
-        newMap[char] = (newMap[char] || 0) + 1;
+        map[char] = (map[char] || 0) + 1;
     }
     for (let char of t){
-       if (!newMap[char]) return false; // char DNE in s
-        newMap[char]--; // subtract count from map
+       if (!map[char]) return false; // char DNE in s
+        map[char]--; // subtract count from map
     }
     return true; // same amount of chars in s & t
 };
