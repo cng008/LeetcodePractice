2ac650bace8882243ae3468ef57ace3ac532cccb
Christien <92824407+cng008@users.noreply.github.com>
2022-11-25 13:06:56 -0800

    Time: 99 ms (78.19%), Space: 44.9 MB (62.91%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -1,22 +1,26 @@
 /**
  * @param {string} s
  * @return {boolean}
- * Time: O(n)
- * Space: O(n)
+ * Time: O(n) -> iterate through the entire string
+ * Space: O(1) -> no extra memory
  */
 const isPalindrome = (s) => {  
-    // ensure characters are lowercase and alphanumeric
-    s = s.toLowerCase().replaceAll(/[^a-zA-Z0-9]/gi, "")
+    s = s.toLowerCase().replaceAll(/[^A-Za-z0-9]/gi, '') // remove non-alpha-num
     let l = 0,
-        r = s.length - 1
+        r = s.length - 1;
     
-    while (l <= r){
+    while (l <= r) {
         if (s[l] !== s[r]) {
             return false
         }
         // do next comparison
-        l++
-        r--
+        l++;
+        r--;
     }
     return true
 };
+
+// BRUTE FORCE
+// remove non-alpha-num
+// compare to reverse
+// problem: creates extra memory to clean up string and to create reverse string
