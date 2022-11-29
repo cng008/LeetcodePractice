df100ea0a34069b3011989b1c3cad53284a36f63
Christien <92824407+cng008@users.noreply.github.com>
2022-11-28 21:33:37 -0800

    Time: 182 ms (83.63%), Space: 52.2 MB (85.26%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -1,42 +1,47 @@
 /**
  * @param {number[]} nums
  * @return {number[][]}
- * Time: O(n^2)
- * Space: O(n)
+ * Time: O(n^2) -> sorting, search pair for every number in the input arr
+ * Space: O(n) -> required for sorting
  */
 const threeSum = (nums) => {
-    nums.sort((a, z) => a - z) // sort array, mutates original -> O(NlogN)
-    const triplets = [] // initialize variable to hold result
+    nums.sort((a, z) => a - z); // sort array, mutates original -> O(NlogN)
+    const results = []; // initialize variable to hold result
     for (i = 0; i < nums.length; i++){
         // skip same consecutive numbers
-        if (i > 0 && nums[i] === nums[i-1]){
+        if (i > 0 && nums[i] === nums[i - 1]){
             continue;
         }
-        searchPair(nums, -nums[i], i + 1, triplets)
+        searchPair(nums, -nums[i], i + 1, results);
     }
-    return triplets
+    return results;
 };
 
-const searchPair = (nums, targetSum, left, triplets) => {
-    let right = nums.length - 1
+const searchPair = (nums, targetSum, left, results) => {
+    let right = nums.length - 1;
     while(left < right){
         // if sum of numbers at left+right pointers equals target
         const currentSum = nums[left] + nums[right]
         if(currentSum === targetSum){
-            triplets.push([-targetSum, nums[left], nums[right]])
-            left++
-            right--
+            results.push([-targetSum, nums[left], nums[right]])
+            left++;
+            right--;
             // checks to avoid duplicate results
             while(left < right && nums[left] === nums[left - 1]){
-                left++ // skip same element to avoid duplicate triplets
+                left++; // skip same element to avoid duplicate triplets
             }
             while(left < right && nums[right] === nums[right + 1]){
-                right-- // skip same element to avoid duplicate triplets
+                right--; // skip same element to avoid duplicate triplets
             }
-        } else if (targetSum > currentSum){
-            left++ // need a pair with a bigger sum
+        } else if (currentSum > targetSum) {
+            right--; // need a pair with a smaller sum
         } else {
-            right-- // need a pair with a smaller sum
+            left++; // need a pair with a bigger sum
         }
     }
 }
+
+
+// BRUTE FORCE O(N^3)
+// 3 nested loops
+// keep track of current sum
