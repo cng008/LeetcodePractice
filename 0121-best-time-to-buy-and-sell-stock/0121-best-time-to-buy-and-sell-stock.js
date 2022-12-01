ea8ec9f5d812713e26e63873586bf6605f1835dd
Christien <92824407+cng008@users.noreply.github.com>
2022-11-30 20:56:38 -0800

    Time: 148 ms (39.46%), Space: 51.8 MB (36.38%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -1,11 +1,12 @@
 /**
  * @param {number[]} prices
  * @return {number}
+ * Time: O(n) -> n is the number of nums in the array
+ * Space: O(1) -> constant space
  */
 const maxProfit = (prices) => {
     let max_profit = 0, 
-        windowStart = 0, // buy
-        windowEnd = 1; // sell
+        windowStart = 0 // buy
     
     for (let windowEnd = 1; windowEnd < prices.length; windowEnd++) {
         if (prices[windowStart] < prices[windowEnd]){
@@ -18,3 +19,6 @@ const maxProfit = (prices) => {
     
     return max_profit
 };
+
+// BRUTE FORCE O(N^2)
+// nested for loop
