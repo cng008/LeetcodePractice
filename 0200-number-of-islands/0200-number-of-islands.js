2447986f263b4f7165171a1a758d55826b9cf44d
Christien <92824407+cng008@users.noreply.github.com>
2022-12-06 21:40:51 -0800

    Time: 157 ms (33.68%), Space: 44.8 MB (79.84%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -1,8 +1,8 @@
 /**
  * @param {character[][]} grid
  * @return {number}
- * Time: O(m*n) → m is number of rows, n is number of columns	
- * Space: O(m*n) → m is number of rows, n is number of columns of the input matrix
+ * Time: O(m * n) → m is number of rows, n is number of columns	
+ * Space: O(m * n) → m is number of rows, n is number of columns of the input matrix
  */
 const numIslands = (grid) => {
     const rows = grid.length,
@@ -11,12 +11,9 @@ const numIslands = (grid) => {
     
     for (let i = 0; i < rows; i++){
         for (let j = 0; j < cols; j++) {
-            // if cell is land
-            if (grid[i][j] == 1){
-                // island found
-                islandCount++;
-                // visit surrounding cells
-                visitIslandsDFS(grid, i, j)
+            if (grid[i][j] == 1){ // cell is land
+                islandCount++; // island found
+                visitIslandsDFS(grid, i, j) // visit surrounding cells
             }
         }
     }
@@ -24,12 +21,9 @@ const numIslands = (grid) => {
 };
 
 const visitIslandsDFS = (grid, row, col) => {
-    // return if not a valid cell
-    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return;
-    // return if cell is water (0)
-    if (grid[row][col] == 0) return;
-    // update input matrix after visiting
-    grid[row][col] = 0;
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return; // not a valid cell
+    if (grid[row][col] == 0) return; // cell is water (0)
+    grid[row][col] = 0; // mark as visited
 
     // recursively visit surrounding cells until there are no surrounding islands
     visitIslandsDFS(grid, row + 1, col); // lower cell
@@ -39,24 +33,21 @@ const visitIslandsDFS = (grid, row, col) => {
 }
 
 // const visitIslandsBFS = (grid, x, y) => {
-//     // start queue
-//     const neighbors = [[x,y]]
-//     while (neighbors.length > 0){
-//         // get first from queue
-//         const cell = neighbors.shift()
-//         const row = cell[0]
-//         const col = cell[1]
-//     // if not a valid cell, return
-//     if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return;
-//     // if cell is water (0), return
-//     if (grid[row][col] == 0) return;
-//     // update input matrix after visiting
-//     grid[row][col] = 0;
+    // const neighbors = [[x,y]]  // start queue
 
-//     // recursively visit surrounding cells until there are no surrounding islands
-//     visitIslandsBFS(grid, row + 1, col); // lower cell
-//     visitIslandsBFS(grid, row - 1, col); // upper cell
-//     visitIslandsBFS(grid, row, col + 1); // right cell
-//     visitIslandsBFS(grid, row, col - 1); // left cell
-//     }
+    // while (neighbors.length > 0){
+    //     const cell = neighbors.shift() // get first from queue
+    //     const row = cell[0]
+    //     const col = cell[1]
+
+    //     if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) return; // not a valid cell
+    //     if (grid[row][col] == 0) return; // cell is water (0)
+    //     grid[row][col] = 0; // mark as visited
+
+    //     // recursively visit surrounding cells until there are no surrounding islands
+    //     visitIslandsBFS(grid, row + 1, col); // lower cell
+    //     visitIslandsBFS(grid, row - 1, col); // upper cell
+    //     visitIslandsBFS(grid, row, col + 1); // right cell
+    //     visitIslandsBFS(grid, row, col - 1); // left cell
+    //     }
 // }
