e062127f96f880af849fc35c63a979b9457fec60
Christien <92824407+cng008@users.noreply.github.com>
2022-12-06 21:49:55 -0800

    Time: 121 ms (21.98%), Space: 44.3 MB (59.71%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -1,18 +1,23 @@
 /**
  * @param {number} n
  * @return {string[]}
+ * Time: O(n) -> factorial
+ * Space: O(n) -> output arr will be length of input
  */
+
 const fizzBuzz = (n) => {
-    let result = []
-    for (let i=1; i<=n; i++){
-        let currentString = "";
+    const res = []; // output array
+    
+    for (let i = 1; i <= n; i++) {
+        let currentStr = '';
         
-        if(i % 3 === 0) currentString += "Fizz";
-        if(i % 5 === 0) currentString += "Buzz";
-
-        if(currentString === "") currentString += i;
-
-        result.push(currentString);
+        if (i % 3 === 0) currentStr += 'Fizz';
+        if (i % 5 === 0) currentStr += 'Buzz';
+        
+        if(currentStr === "") currentStr += i; // neither divisible by 3 nor 5
+        
+        res.push(currentStr); // push to output array
     }
-    return result
+    
+    return res;
 };
