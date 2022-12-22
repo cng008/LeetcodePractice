01cc23618cdab99c08f997a63277286d90af6990
Christien <92824407+cng008@users.noreply.github.com>
2022-12-21 22:00:05 -0800

    Time: 79 ms (86.53%), Space: 44.8 MB (28.52%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -7,10 +7,10 @@
 const missingNumber = (nums) => {
     let i = 0;
     
-    while(i < nums.length){
+    while(i < nums.length) {
         let j = nums[i];
-        // if current bumber is less than length of array and current # is not at correct index
-        if(nums[i] < nums.length && nums[i] !== nums[j]){
+        // if current number is less than length of array and current # is not at correct index
+        if (nums[i] < nums.length && nums[i] !== nums[j]) {
             [nums[i], nums[j]] = [nums[j], nums[i]]; // swap
         } else {
             i++; // increment if the current index has the correct number
@@ -18,11 +18,10 @@ const missingNumber = (nums) => {
     }
     
     // now that most numbers match index, iterate to find which index has wrong #
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i] !== i){
-            return i;
-        }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i) return i;
     }
     
     return nums.length;
+
 };
