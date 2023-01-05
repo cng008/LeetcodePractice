ee51b4ec9ec7791bd1e4191e4b84207d5f6c4130
Christien <92824407+cng008@users.noreply.github.com>
2023-01-04 20:22:44 -0800

    Time: 157 ms (19.72%), Space: 49 MB (66.95%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -14,21 +14,19 @@
  and m is the number of nodes in the tree t	
  * Space: O(m) -> at most, the recursive stack will have m levels
  */
-const isSubtree = (s, t) => {
-    if (!s) return false; // no subtree
-    if (isSameTree(s, t)) return true; // current node in s is a subtree
+const isSubtree = (s, t) => {    
+    if (!s) return false; // if the current node is null, t cannot be a subtree of s
+    if (isSameTree(s, t)) return true; // check if current subtree is the same as t
     // recursively check if t is a subtree of s's left or right children
     return isSubtree(s.left, t) || isSubtree(s.right, t);
 };
 
-const isSameTree = (s, t) => {
-    if (!s && ! t) return true; // edge case: trees are the same when both null
+const isSameTree = (p, q) => {    
+    if (!p && ! q) return true; // edge case: trees are the same when both null
     
+    // either s and t is null or values at the current node are different
+    if (!p || !q || p.val !== q.val) return false;
     // nodes exist and values at the current node are same
-    if (s && t && s.val === t.val) {
-        // recursively check if the left and right subtrees are the same
-        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
-    }
-    // if only one of s and t is null or if the values at the current node are different, the trees are not the same
-    return false;
-}
+    // recursively check if the left and right subtrees are the same
+   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
