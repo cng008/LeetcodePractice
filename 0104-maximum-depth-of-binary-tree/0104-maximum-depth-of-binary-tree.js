2c2d4f090e776486b96533fea2dc1c0b113a1507
Christien <92824407+cng008@users.noreply.github.com>
2023-01-04 17:02:07 -0800

    Time: 116 ms (28.47%), Space: 45.4 MB (34.01%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -12,28 +12,48 @@
  * Time: O(n) -> traverse over every node
  * Space: O(n) -> for queue
  */
+
+// BFS
 const maxDepth = (root) => {
     if (!root) return 0; // edge case
     
-    const queue = [];
-    queue.push(root);
+    const queue = []; // create a queue to store nodes as we visit them
+    queue.push(root); // add root node to queue
     let maxDepth = 0; // track number of iterations
     
-    while(queue.length > 0){
-        maxDepth++;
-        const levelSize = queue.length;
-        
+    // continue looping as long as there are nodes in the queue
+    while(queue.length > 0) {
+        maxDepth++; // increment for each iteration   
+        const levelSize = queue.length; // store the size of the current level
+        // loop through all nodes in the current level (queue)
         for (let i = 0; i < levelSize; i++){
+            // remove the first node from the queue and assign it to currentNode
             const currentNode = queue.shift();
             
-            // add child nodes of currentNode to queue
-            if (currentNode.left){
-                queue.push(currentNode.left);
-            }
-            if (currentNode.right){
-                queue.push(currentNode.right);
-            }
+            // add child nodes of currentNode to queue (if exists)
+            if (currentNode.left) queue.push(currentNode.left);
+            if (currentNode.right) queue.push(currentNode.right);
         }
     }
     return maxDepth;
 };
+
+
+/** DFS 
+ * Time: O(n) -> traverse over every node
+ * Space: O(n) -> recursive call stack
+ */
+// const maxDepth = (root) => {
+//     if (!root) return 0; // edge case
+
+//     return recursiveDFS(root)
+// };
+
+// const recursiveDFS = (root) =>{
+//     const left = maxDepth(root.left)
+//     const right = maxDepth(root.right)
+//     const height = Math.max(left, right)
+
+//     return height + 1
+
+// }
