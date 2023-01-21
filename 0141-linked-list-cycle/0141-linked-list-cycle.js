27eb9710bea56965d38f5539f7970eeb0d532d48
Christien <92824407+cng008@users.noreply.github.com>
2023-01-20 22:31:15 -0800

    Time: 68 ms (96.32%), Space: 44.4 MB (82.49%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -9,23 +9,24 @@
 /**
  * @param {ListNode} head
  * @return {boolean}
- * Time: O(n) -> traverse entire linked list
- * Space: O(1) -> no extra memory
+ input: head of LL
+ outpu: bool, t if cycle exists
+ Time: O(n) -> traverse entire LL if no cycle
+ Space: O(1) -> no mem
  */
+// FAST SLOW POINTERS
 const hasCycle = (head) => {
     // start at the head for both pointers
     let slow = head,
         fast = head;
     
     // only need to check fast pointer bc it will be the one to reach first
-    while (fast && fast.next){ 
+    while (fast && fast.next) {
         slow = slow.next; // moves 1
         fast = fast.next.next; // moves 2
         
-        if (fast === slow){
-            return true; // cycle found
-        }
+        // finding a cycle. fast eventually catches up to slow
+        if (fast === slow) return true; // cycle found
     }
-    
-    return false;
+    return false; // did not encounter a cycle
 };
