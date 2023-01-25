bb317ade77985f20c9352bfa9801d38a6fea09a2
Christien <92824407+cng008@users.noreply.github.com>
2023-01-25 11:45:53 -0800

    Time: 71 ms (91.94%), Space: 44.4 MB (82.92%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -1,15 +1,18 @@
 /**
  * @param {number[]} nums
  * @return {number}
- * Time: O(n) -> iterate through entire array once
- * Space: O(1) -> modify in-place, no extra memory
+input: arr (ints, pos, neg, dupes)
+output: int (length of unique part of the array)
+Time: O(n) -> iterate through entire array once
+Space: O(1) -> modify in-place, no extra memory
  */
 const removeDuplicates = function(nums) {
     if (nums.length === 0) return 0;
      
     let i = 0; // keep track of the current position in the array
     
-    for (let j = 0; j < nums.length; j++) {
+    for (let j = 1; j < nums.length; j++) {
+        // checks if unique
         if (nums[i] !== nums[j]) {
             i++;
             nums[i] = nums[j]; // swap
