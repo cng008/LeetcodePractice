d76014e9d9be07fb8fad2ffd30db21ee5d243724
Christien <92824407+cng008@users.noreply.github.com>
2023-01-26 21:16:51 -0800

    Time: 118 ms (13.78%), Space: 44.6 MB (17.88%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -1,23 +1,29 @@
 /**
  * @param {number} n
  * @return {string[]}
- * Time: O(n) -> factorial
- * Space: O(n) -> output arr will be length of input
+input: int
+output: arr of str, length of input num
+Time: O(n)
+Space: O(n)
+input and output values ints/str?
+output order consecutive matter? 1 to n or n to 1
+Should the output contain the input value if it is not divisible by 3 or 5? if it is?
+is the input range inclusive or exclusive of the endpoints?
  */
-
+// 3 & 5 -> FizzBuzz
+// 3 -> Fizz
+// 5 -> Buzz
+// none -> current number in iteration
 const fizzBuzz = (n) => {
-    const res = []; // output array
-    
-    for (let i = 1; i <= n; i++) {
-        let currentStr = '';
-        
-        if (i % 3 === 0) currentStr += 'Fizz';
-        if (i % 5 === 0) currentStr += 'Buzz';
-        
-        if(currentStr === "") currentStr += i; // neither divisible by 3 nor 5
-        
-        res.push(currentStr); // push to output array
+    const res = [];
+
+   for (let i = 1; i <= n; i++) { 
+        let str = '';
+
+        if (i % 3 === 0) str += 'Fizz';
+        if (i % 5 === 0) str += 'Buzz';
+        if (str === '') str += i; // neither divisible by 3 nor 5
+        res.push(str);
     }
-    
     return res;
-};
+}
