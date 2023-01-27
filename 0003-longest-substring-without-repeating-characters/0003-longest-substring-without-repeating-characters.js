2612eb833d2eed8ac73af092dd5f48e3cdc2a900
Christien <92824407+cng008@users.noreply.github.com>
2023-01-27 11:26:34 -0800

    Time: 114 ms (58.36%), Space: 47.4 MB (60.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -1,15 +1,18 @@
 /**
  * @param {string} s
  * @return {number}
- * assumptions: str is not empty, lowercase letters only
- * output: integer, length of longest substring of distinct characters
+input: str
+output: longest substr of repeating chars (int)
+Time: O(n)
+Space: O(n)
  */
 // SLIDING WINDOW, HASH MAP
 const lengthOfLongestSubstring = (s) => {
-    let maxLength = 0,
-        windowStart = 0,
-        charIndexMap = {};
+    let maxLength = 0, // store the maximum length of a substring
+        windowStart = 0, // store the start index of the current substring
+        charIndexMap = {}; // stores the last index of each character
     
+    // tterate through each char in the string
     for (let windowEnd = 0; windowEnd < s.length; windowEnd++){
         const rightChar = s[windowEnd]
         if (rightChar in charIndexMap) {
@@ -17,15 +20,15 @@ const lengthOfLongestSubstring = (s) => {
             // update start to include repeating char
             windowStart = Math.max(windowStart, charIndexMap[rightChar]+1)
         }
-        // insert rightChar into the map
+         // update the map with the current character's index
         charIndexMap[rightChar] = windowEnd
-        // remember the max length so far
+        // update maxLen of a substring if the curr substring's length > current maximum
         maxLength = Math.max(maxLength, windowEnd - windowStart + 1)
     }
     return maxLength
 };
 
 
-// BRUTE FORCE O (n * k)
-// n is total number of elements in the given array, k is size of subarray (if declared)
+// BRUTE FORCE O (n^3) -> for each character in the input string, we are generating all possible substrings starting from that character and checking each one for repeating characters
 // nested for loop
+// generate all possible substrings of the input string, and check for each substring if it contains any repeating characters. If a substring does not contain any repeating characters, then we can compare its length with the current maximum length and update the maximum length if necessary
