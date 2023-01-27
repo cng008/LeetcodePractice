8d312154584f81e149bd2760187ce1255a87a08d
Christien <92824407+cng008@users.noreply.github.com>
2023-01-26 21:45:36 -0800

    Time: 76 ms (67.87%), Space: 45.2 MB (71.89%) - LeetHub

--- 0112-path-sum/0112-path-sum.js
+++ 0112-path-sum/0112-path-sum.js
@@ -10,18 +10,20 @@
  * @param {TreeNode} root
  * @param {number} targetSum
  * @return {boolean}
- * Time: O(n) -> traverse over every node once
- * Space: O(n) -> for recursion stack
+input: root of tree (pos/neg, binary, can be empty), target (int)
+output: bool, t if target sum found
+Time: O(n) -> traverse each node
+Space: O(n) -> recursive call stack
  */
-const hasPathSum = (root, targetSum) => {
+// DFS to recursively traverse down each path
+// once we hit leaf node, backtrack and add up sum
+const hasPathSum = function(root, targetSum) {
     if (!root) return false; // edge case
-    
+
     // current node is a leaf node AND its value is equal to the sum
-    if (root.val === targetSum && !root.left && !root.right){
-        return true;
-    }
+    if (root.val === targetSum && !root.left && !root.right) return true;
     
-    // recursively call to traverse the left and right sub-tree 
+    // recursively call to traverse the left and right sub-tree, subtracting from targetSum at each iteration
     // stops if any of the calls return true
     return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
 };
