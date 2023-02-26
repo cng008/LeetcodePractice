8ce16dc27d8d48dca4d82e8f660e923604799f5a
Christien <92824407+cng008@users.noreply.github.com>
2023-02-25 21:26:58 -0800

    Time: 65 ms (81.54%), Space: 45.1 MB (26.26%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -2,27 +2,30 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
- * Time: O(log n) -> each time the search is run, length is divided by 2
- * Space: O(1) -> only need two variables to keep track of the range of elements that are to be checked
+input: arr (ints, pos/neg, sorted, unique, non-empty)
+output: int (pos/neg); return -1 if not found
+Time: O(log n) -> binary search
+Space: O(n) -> no extra mem
  */
 const search = (nums, target) => {
     let l = 0,
         r = nums.length - 1;
-    
-    while (l <= r){
-        let mid = Math.floor(( l + r) / 2); // can't have float as midpoint
+
+    while (l <= r) {
+        const mid = Math.floor((l + r) / 2); // can't have float as midpoint
         // let mid = l + ((r - l) >> 2); // bitwise right shift operator (faster for larger sums)
         
-        if (nums[mid] < target){
+        if (nums[mid] === target) { // target found
+            return mid;
+        } else if (nums[mid] < target) {
             l = mid + 1; // search right half
-        } else if (nums[mid] > target) {
+        } else { // target < nums[mid]
             r = mid - 1; // search left half
-        } else {
-            return mid; // target found
         }
     }
+
     return -1; // target not found
-}
+};
 
 
 // BRUTE FORCE O(n)
