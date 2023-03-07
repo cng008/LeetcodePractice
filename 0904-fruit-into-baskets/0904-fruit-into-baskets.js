2ecbaf81660b0c2a2892e0273028d6088acb3ef8
Christien <92824407+cng008@users.noreply.github.com>
2023-03-06 22:22:59 -0800

    Time: 242 ms (28.20%), Space: 54.9 MB (37.38%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.js
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.js
@@ -1,29 +1,33 @@
 /**
  * @param {number[]} fruits
  * @return {number}
- * Time: O(n) -> number of elements in input array
- * Space: O(1) -> can only be a max of 3 types of fruit store in the map
- */
+input: arr (int, unsorted, pos)
+output: int (pos)
+Time: O(n) -> number of elements in input array
+Space: O(1) -> can only be a max of 2 types of fruit store in the map
+*/
 const totalFruit = (fruits) => {
     let windowStart = 0,
         maxLength = 0,
-        freqMap = {};
+        freqMap = {},
+        numDistinctFruits = 0;
     
     // loop through array
     for (let windowEnd = 0; windowEnd < fruits.length; windowEnd++){
         const rightFruit = fruits[windowEnd]
-        // save to hash map
-        if(!(rightFruit in freqMap)){
-            freqMap[rightFruit] = 0
+        if (!(rightFruit in freqMap)) {
+            numDistinctFruits++;
         }
-        freqMap[rightFruit]++
+        // save to hash map
+        freqMap[rightFruit] = (freqMap[rightFruit] || 0) + 1;
         
         // shrink window when our hash map has more than 2 types of fruit
-        while (Object.keys(freqMap).length > 2){
+        while (numDistinctFruits > 2) {
             const leftFruit = fruits[windowStart]
             freqMap[leftFruit]--
             if(freqMap[leftFruit] === 0){
                 delete freqMap[leftFruit]
+                numDistinctFruits--;
             }
             windowStart++
         }
@@ -39,3 +43,4 @@ const totalFruit = (fruits) => {
 // loop through fruits arr and add to newArr
 // nested loop to check if el in fruits arr is different from newArr
 // shift if true, push if false
+
