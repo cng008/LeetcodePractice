5e0de961cd432e252065d3024e9f8ecd3a4b66cc
Christien <92824407+cng008@users.noreply.github.com>
2023-03-11 20:47:02 -0800

    Time: 56 ms (89.31%), Space: 42.5 MB (10.33%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -2,8 +2,11 @@
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
- * Time: O(n) -> iterate through entire array once
- * Space: O(1) -> modify in-place, no extra memory
+input: arr (int, +/-, dupes ok, unsorted)
+output: int (+, length of arr w/o val, order doesn't matter)
+Time: O(n) -> iterate through entire array once
+Space: O(1) -> modify in-place, no extra memory
+edge case: empty input -> return 0
  */
 const removeElement = (nums, val) => {
     if (nums.length === 0) return 0; // edge case
@@ -12,9 +15,9 @@ const removeElement = (nums, val) => {
     
     for (let j = 0; j < nums.length; j++) {
         if (nums[j] !== val) {
-            nums[i] = nums[j];
+            nums[i] = nums[j]; // move !val to front of input
             i++;
         }
     }
-    return i;
+    return i; // length of arr w/o element
 }
