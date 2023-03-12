da9b118d0ed809fdf50cb946843d0ff151ce0bd7
Christien <92824407+cng008@users.noreply.github.com>
2023-03-11 22:52:23 -0800

    Time: 65 ms (87.61%), Space: 42.7 MB (45.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -2,30 +2,37 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- * Time: O(n) -> iterate through input array
- * Space: O(n) -> hash map, sacrificed space for time improvement
+input: arr (int, +/-, unsorted, dupes ok), target (int, +/-)
+output: arr (pair of idx where the el at the idx sum to target)
+Time: O(n) -> iterate once
+Space: O(n) -> hash map
+edge cases: empty input
  */
 const twoSum = (nums, target) => {
     const map = new Map();
     for (let i = 0; i < nums.length; i++) {
-        if (map.has(target - nums[i])){ // difference of target exists in map
-            return [map.get(target - nums[i]), i] // returns indices
-        }
+        if (map.has(target - nums[i])) return [map.get(target - nums[i]), i] // returns indices
         map.set(nums[i], i) // set number as key and indice as value
-    }
-};
+     }
+}
 
-
-/** BRUTE FORCE
-* Time: O(N^2) | Space: O(1)
-*/
+// ALT SOLUTON
 // const twoSum = (nums, target) => {
-//     for (let i = 0; i < nums.length; i++){
-//         for (let j = 0; j < nums.length; j++){
-//             if (nums[i] + nums[j] === target){
-//                 return [i, j]
-//             }
+//    let numMap = {};
+
+//    for (let i = 0; i < nums.length; i++){
+//        let num = nums[i];
+
+//        if ((target - num) in numMap) return [numMap[target - num], i];
+//        numMap[num] = i;
+//    }
+// }
+
+// BRUTE FORCE Time O(n^2) | Space: O(1)
+// const twoSum = (nums, target) => {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] === target) return [i, j]
 //         }
-//     }
-//     // assumed that there is a solution
-// };
+//      }
+// }
