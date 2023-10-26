daae5017268ff427ab3a5c67d15ff395107bd535
Christien Ng <92824407+cng008@users.noreply.github.com>
2023-10-26 16:25:49 -0700

    Time: 41 ms (98.56%), Space: 42.6 MB (44.82%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -1,28 +1,27 @@
 /**
  * @param {string} s
  * @return {boolean}
- *
- input: str of brackets
- output: bool, t if brackets close in correct order
- Time: O(n) -> iterate entire str
- Space: O(n) -> stack
+input: str (brackets, can be (), [], {})
+output: bool (t if valid closing order)
+Time: O(n) -> check all el in str
+Space: O(n) -> call stack
+edge cases: empty str, invalid chars
  */
 const isValid = (s) => {
     const pairs = {'(' : ')', '{' : '}', '[' : ']'}, // map of bracket pairs
-          stack = []; // keeps track of closing brackets
+        stack = []; // keeps track of closing brackets
     
     for (let char of s) {
         if (char in pairs) { // character is an open bracket
             stack.push(pairs[char]); // add corresponding closing bracket to the stack
-        } else if (stack.length > 0 && stack[stack.length - 1] === char) {  // character is a closing bracket and matches the last bracket in the stack
+        } else if (stack.length > 0 && stack[stack.length-1] === char) { // character is a closing bracket and matches the last bracket in the stack
             stack.pop(); // remove the last bracket from the stack
-        } else {  // character is a closing bracket but does not match the last bracket in the stack
+        } else { // character is a closing bracket but does not match the last bracket in the stack
             return false; // input string is not valid parens
         }
     }
     return stack.length === 0; // stack is empty, meaning all brackets are correctly paired
 };
-
 
 // ALTERNATE SOLUTION
 // const isValid = (s) => {
