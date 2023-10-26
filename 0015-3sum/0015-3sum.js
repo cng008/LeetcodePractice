fa991741248f5715b3a17088a56e7741f7e146db
Christien Ng <92824407+cng008@users.noreply.github.com>
2023-10-25 17:50:36 -0700

    Time: 145 ms (80.35%), Space: 59.4 MB (34.52%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -1,47 +1,45 @@
 /**
  * @param {number[]} nums
  * @return {number[][]}
- * Time: O(n^2) -> sorting, search pair for every number in the input arr
- * Space: O(n) -> required for sorting
+input: arr (int, +/-, unsorted, nonempty)
+output: arr (triplets)
+Time: O(nlogn)+O(n^2)=>O(n^2) -> sorting, search pair for every number in the input arr
+Space: O(n) -> required for sorting
+edge cases: 
  */
 const threeSum = (nums) => {
     nums.sort((a, z) => a - z); // sort array, mutates original -> O(NlogN)
-    const results = []; // initialize variable to hold result
-    for (i = 0; i < nums.length; i++){
+    const res = []; // initialize variable to hold result
+
+    for (let i = 0; i < nums.length; i++) {
         // skip same consecutive numbers
-        if (i > 0 && nums[i] === nums[i - 1]){
-            continue;
-        }
-        searchPair(nums, -nums[i], i + 1, results);
+        if (i > 0 && nums[i] === nums[i-1]) continue; // not first value in nums && same value as before
+        findPair(nums, -nums[i], i+1, res);
     }
-    return results;
+    return res;
 };
 
-const searchPair = (nums, targetSum, left, results) => {
-    let right = nums.length - 1;
-    while(left < right){
-        // if sum of numbers at left+right pointers equals target
-        const currentSum = nums[left] + nums[right]
-        if(currentSum === targetSum){
-            results.push([-targetSum, nums[left], nums[right]])
-            left++;
-            right--;
-            // checks to avoid duplicate results
-            while(left < right && nums[left] === nums[left - 1]){
-                left++; // skip same element to avoid duplicate triplets
-            }
-            while(left < right && nums[right] === nums[right + 1]){
-                right--; // skip same element to avoid duplicate triplets
-            }
-        } else if (currentSum > targetSum) {
-            right--; // need a pair with a smaller sum
+const findPair = (nums, targetSum, l, res) => {
+    let r = nums.length - 1;
+    while (l < r) {
+        const currSum = nums[l] + nums[r];
+        if (currSum === targetSum) { // base case
+            res.push([-targetSum, nums[l], nums[r]]);
+            l++;
+            r--;
+            // skip same element to avoid duplicate triplets
+            while (nums[l] === nums[l-1]) l++;
+            while (nums[r] === nums[r+1]) r--;
+        } else if (currSum < targetSum) {
+            l++; // need a pair with a bigger sum
         } else {
-            left++; // need a pair with a bigger sum
+            r--; // need a pair with a smaller sum
         }
     }
 }
 
+// ALT O(n^2)
+// hash map with nested for loop
 
-// BRUTE FORCE O(N^3)
-// 3 nested loops
-// keep track of current sum
+// BRUTE FORCE O(n^3)
+// triple nested loop
